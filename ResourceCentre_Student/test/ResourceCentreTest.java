578e170e9ba59f691d6e430c62f37fd1291766f1
19007162 <19007162@DESKTOP-6CL65KU>
2020-07-21 13:58:58 +0800

    Update on viewAllChromebook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -111,8 +111,8 @@ public class ResourceCentreTest {
 				//test if the expected output string same as the list of chromebooks retrieved from the SourceCentre
 				allChromebook= ResourceCentre.retrieveAllChromebook(chromebookList);
 
-				testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n","CC0011", "Nikon HDSLR", "Yes", "", 40);
-				testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n","CC0012", "Sony DSC-RX100M7", "Yes", "", 20);
+				testOutput = String.format("%-10s %-30s %-10s %-10s %-20d\n","CB0011", "My Google Chromebook 1st", "Yes", "", "Mac OS");
+				testOutput += String.format("%-10s %-30s %-10s %-10s %-20d\n","CB0012", "SAMSUNG Chromebook 4+", "Yes", "", "Win 10");
 			
 				assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 	}
